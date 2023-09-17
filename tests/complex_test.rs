adfccd5e23ed3399fde76e4902441924d9e4ba35
AlephEleven <lambdasauce2@gmail.com>
2023-09-17 03:21:30 -0400

    display and mul tests

--- tests/complex_test.rs
+++ tests/complex_test.rs
@@ -10,6 +10,15 @@ mod complex_test {
     }
 
     #[test]
+    fn display() {
+        let c1 = Complex::new(1.0, 2.0);
+        let c2 = Complex::new(1.0, -2.0);
+        assert_eq!("1.0+2.0i", c1.to_string());
+        assert_eq!("1.0-2.0i", c2.to_string());
+
+    }
+
+    #[test]
     fn conj() {
         let c = Complex::new(1.0, 2.0);
         assert_eq!(c.conj(), Complex{re: 1.0, im: -2.0});
@@ -19,6 +28,21 @@ mod complex_test {
     fn add() {
         let c1 = Complex::new(1.0, 2.0);
         let c2 = Complex::new(2.0, 3.0);
-        assert_eq!(c1+c2, Complex{re: 2.0, im: 5.0});
+        assert_eq!(c1+c2, Complex{re: 3.0, im: 5.0});
     }
+
+    #[test]
+    fn sub() {
+        let c1 = Complex::new(1.0, 2.0);
+        let c2 = Complex::new(2.0, 3.0);
+        assert_eq!(c1-c2, Complex{re: -1.0, im: -1.0});
+    }
+
+    #[test]
+    fn mul() {
+        let c1 = Complex::new(1.0, 2.0);
+        let c2 = Complex::new(2.0, 3.0);
+        assert_eq!(c1*c2, Complex{re: -4.0, im: 7.0});
+    }
+
 }
